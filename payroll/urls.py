847c2b25c4cdfca430c6a20126f0bd5466427c02
Angelo Gonzalez <angelo3649@icloud.com>
2024-02-26 18:23:52 -0500

    slight changes

--- payroll/urls.py
+++ payroll/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from payrollapp.views import landing_page
+from payrollapp.views import landing_page, calculate_payroll
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('payroll/', include('payrollapp.urls')),  # Include app-level URLs
     path('', landing_page, name='landing_page'),
+     path('calculator/', calculate_payroll, name='calculator_page'),
 ]
